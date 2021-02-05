847be1019e7020166e4e2bf3476ce6ccfc910324
emmanuel <oliveiravianaviana@gmail.com>
2021-02-05 02:52:04 -0300

    Commit Madruga

--- server/index.js
+++ server/index.js
@@ -14,3 +14,24 @@ const io = socketIo(server, {
         credentials: false
     }
 })
+
+io.on('connection', socket => {
+    console.log('connection', socket.id)
+    socket.on('join-room', (roomId, userId) => {
+        //adciona os usuarios na sala
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+        socket.on('disconnect', () => {
+            console.log('disconnected', roomId, userId)
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        })
+    })
+})
+
+
+const startServer = () => {
+    const { address, port } = server.address()
+    console.info(`app running at ${address}:${port}`)
+}
+
+server.listen(process.env.PORT | 3000, startServer)
